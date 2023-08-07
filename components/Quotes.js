bd6c37cf2d2b9d3a173bea44641a648b884a69c1
owo93 <Iztyle30@gmail.com>
2023-08-07 20:45:17 +0700

    Anchor NavBar to top of page

--- components/Quotes.js
+++ components/Quotes.js
@@ -1,6 +1,6 @@
 export default function Quotes() {
   return (
-    <div className="w-full bg-white py-16 px-4">
+    <div className="w-full h-screen bg-white py-16 px-4">
       <h1>Place example situations/quotes here</h1>
     </div>
   );
