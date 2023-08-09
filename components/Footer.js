6b041ce84e616965c597d98e0fe41ffce98c2c2c
owo93 <Iztyle30@gmail.com>
2023-08-10 00:48:40 +0700

    Not-so minor changes

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,21 @@
 import Image from "next/image";
+import ike from "/public/images/ike.png";
+import nasa from "/public/images/nasa.png";
+import peak from "/public/images/peak.png";
+import sun from "/public/images/sun.png";
+import trust from "/public/images/trust.png";
 
 export default function Footer() {
   const names = ["Ike", "Trust", "Sun", "Peak", "Nasa"];
   const roles = [
-    "Developer & Assistant Manager",
+    "Developer",
     "Project Manager & UI/UX",
     "Developer",
     "Developer",
     "UI/UX",
   ];
+
+  const images = [ike, trust, sun, peak, nasa];
 
   return (
     <div className="mx-auto w-full max-w-7xl items-center px-5 py-24 md:px-12 lg:px-16">
@@ -18,8 +25,8 @@ export default function Footer() {
             <div className="flex flex-col items-center gap-4 text-center">
               <Image
                 className="h-16 w-16 rounded-full object-cover lg:h-20 lg:w-20"
-                src=""
-                alt=""
+                src={images[index]}
+                alt={name}
               />
               <div className="space-y-1">
                 <h3 className="text-lg font-medium leading-6 text-black">
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Card from "@/components/Card";
 import Hero from "@/components/Hero";
 import Quotes from "@/components/Quotes";
 import SDG from "@/components/SDG";
@@ -41,6 +42,23 @@ export default function Home() {
         "By acknowledging our profound interrelation with nature and appreciating even the smallest overlooked aspects, we cultivate the readiness to contribute positively to our world.",
     },
   ];
+  const initiatives = [
+    {
+      title: "Alternative Energy",
+      body: "Alternative energy allows us to transition from polluting fossil fuels to significantly cleaner renewable energy.",
+      imageUrl: "",
+    },
+    {
+      title: "Waste Management",
+      body: "Good waste management enables us to appropriately dispose of trash that would otherwise leak and harm the environment.",
+      imageUrl: "",
+    },
+    {
+      title: "Creating a better enviroment",
+      body: "Good environment is necessary for maintaining good mental and physicals health.",
+    },
+    // Add more initiatives as needed
+  ];
 
   return (
     <Layout>
@@ -56,6 +74,22 @@ export default function Home() {
         imageSrc={UN}
         details="SDG 13 is a United Nations Sustainable Development Goal that aims to take urgent action to combat climate change and its impacts."
       />
+      <div className="px-12 pt-4 text-[#1D2130]">
+        <p className="text-base">Around the country</p>
+        <h1 className="text-xl font-semibold">
+          Initiatives to combat climate change in Thailand
+        </h1>
+      </div>
+      <div className="grid grid-cols-1 gap-4 pt-4 sm:gap-2 md:grid-cols-2 md:px-8 lg:grid-cols-3 lg:px-24">
+        {initiatives.map((initiative, index) => (
+          <Card
+            key={index}
+            title={initiative.title}
+            body={initiative.body}
+            imageUrl={initiative.imageUrl}
+          />
+        ))}
+      </div>
       {/* Statistics here*/}
       {/* Initiatives here*/}
       <Quotes quotes={quotesData} />
