1c8b6088f0079bd09a153a6d45490fef8df3baf5
Sun-52 <102531810+Sun-52@users.noreply.github.com>
2023-08-10 13:39:50 +0700

    Minor change

--- components/Statistics.js
+++ components/Statistics.js
@@ -3,11 +3,10 @@ import Image from "next/image";
 
 export function Stat({ fact }) {
   return (
-    <div className="h-[593px] w-full relative items-center">
-      <div className="text-orange-500 text-8xl font-bold leading-96px z-50">
+    <div className="h-auto w-full relative items-center jus">
+      <div className="text-orange-500 text-4xl font-bold leading-96px z-50 p-4">
         459.9 metric tons
       </div>
-
       <Image
         className="z-20 opacity-30"
         src="/images/fact.png"
