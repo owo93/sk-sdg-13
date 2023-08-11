ff62813ff09204076c1f2eb38650ab60873da4ca
owo93 <Iztyle30@gmail.com>
2023-08-11 15:34:33 +0700

    Fixed height quotes

--- components/Quotes.js
+++ components/Quotes.js
@@ -54,7 +54,7 @@ export default function Quotes() {
 
   return (
     <div className="mx-auto flex w-full flex-col items-center">
-      <div className="inline-flex max-h-[300px] w-[500px] flex-col items-start justify-start gap-4 overflow-hidden rounded-2xl border border-gray-200 bg-white p-6 shadow-md">
+      <div className="inline-flex h-[300px] w-[500px] flex-col items-start justify-start gap-4 overflow-hidden rounded-2xl border border-gray-200 bg-white p-6 shadow-md">
         <div className="flex items-center gap-4">
           <div className="relative h-16 w-16 overflow-hidden rounded-full">
             <Image
